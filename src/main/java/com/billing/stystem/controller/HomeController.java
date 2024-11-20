4b58cb0f6085188d9324903e98b0530e51e75768
baki-511 <johanbaki381@gmail.com>
2024-11-20 19:22:58 +0530

    Created Add product and category front end

--- src/main/java/com/billing/stystem/controller/HomeController.java
+++ src/main/java/com/billing/stystem/controller/HomeController.java
@@ -1,11 +1,11 @@
 package com.billing.stystem.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class HomeController {
-    
     
     @GetMapping("/")
     public String home() {
